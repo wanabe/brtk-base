eea9d02fdd1bfb53429b014da4508cf5b4822f36
wanabe <s.wanabe@gmail.com>
2016-03-14 00:43:40 +0900

    Import from brtk

---
+++ mrbgem.rake
@@ -0,0 +1,6 @@
+MRuby::Gem::Specification.new('brtk-base') do |spec|
+  spec.license = 'MIT'
+  spec.authors = 'wanabe'
+
+  spec.add_dependency "mruby-gtk3"
+end
---
+++ mrblib/brtk/base.rb
@@ -0,0 +1,27 @@
+class Brtk
+  module Base
+    class Widget
+      attr_accessor :widget
+
+      def children
+        @children ||= []
+      end
+
+      def add(other)
+        children << other
+        widget.add other.widget
+      end
+
+      def setup(klass, *args)
+        @widget = klass.new(*args)
+      end
+    end
+
+    class Layout < Widget
+      def pack_start(other, *args)
+        children << other
+        widget.pack_start other.widget, *args
+      end
+    end
+  end
+end
